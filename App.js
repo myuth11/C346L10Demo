9b8f04e1baa0641160cf1cae64a7ab02d8c701fd
myu <23029169@myrp.edu.sg>
2025-01-16 10:22:59 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StatusBar, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+let originalData = [];
+const App = () => {
+    const [mydata, setMydata] = useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        // Fetch data only once on first render
+        fetch("https://jsonplaceholder.typicode.com/albums/")
+            .then((response) => response.json())
+            .then((myJson) => {
+                if (originalData.length<1){
+                setMydata(myJson);
+                originalData = myJson;}
+            })
+            .catch((error) => {
+                console.error("Error fetching data: ", error);
+            });
+    }, []); // The empty dependency array ensures this runs only once
+
+    const FilterData = (text) => {
+        if (text!=''){
+            let myFilteredData=originalData.filter((item)=>
+            item.title.includes(text));
+            setMydata(myFilteredData);
+        }
+        else {
+            setMydata(originalData);
+        }
+    }
+
+    const renderItem = ({ item, index }) => {
+        return (
+            <View>
+                <Text style={{ borderWidth: 1 }}>{item.title}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <View>
+            <StatusBar />
+            <Text>Search:</Text>
+            <TextInput style={{ borderWidth: 1 }} onChangeText={(text)=>{FilterData(text)}} />
+            <FlatList data={mydata} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} />
+        </View>
+    );
+};
+
+export default App;
